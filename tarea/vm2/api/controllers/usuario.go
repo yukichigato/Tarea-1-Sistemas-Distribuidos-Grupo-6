d5c1983e8bb9367fcaf0db02ed5519b4d4bc4475
seba3011 <95301427+seba3011@users.noreply.github.com>
2025-09-25 21:34:57 -0300

    error ruta usuario.go

--- tarea/vm2/api/controllers/usuario.go
+++ tarea/vm2/api/controllers/usuario.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/yukichigato/Tarea-1-Sistemas-Distribuidos-Grupo-6/api/models"
-	"github.com/yukichigato/Tarea-1-Sistemas-Distribuidos-Grupo-6/api/models/structs"
-	"github.com/yukichigato/Tarea-1-Sistemas-Distribuidos-Grupo-6/api/utils"
+	"github.com/yukichigato/Tarea-1-Sistemas-Distribuidos-Grupo-6/tarea/vm2/api/models"
+	"github.com/yukichigato/Tarea-1-Sistemas-Distribuidos-Grupo-6/tarea/vm2/api/models/structs"
+	"github.com/yukichigato/Tarea-1-Sistemas-Distribuidos-Grupo-6/tarea/vm2/api/utils"
 )
 
 // Handler para registrar usuario
